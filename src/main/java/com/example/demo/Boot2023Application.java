c31ca30dcd4f2edc780bac36d705146f06b0e4cb
kangjs3779 <123609533+kangjs3779@users.noreply.github.com>
2023-04-11 15:14:00 +0900

    firsr

---
+++ src/main/java/com/example/demo/Boot2023Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot2023Application.class, args);
+	}
+
+}
